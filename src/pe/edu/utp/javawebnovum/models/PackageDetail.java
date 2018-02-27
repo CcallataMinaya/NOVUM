8bcb3bd9627c5c9ab55dd45c5c78a1b2c7b5c3ba
PieroRojas <1310774@utp.edu.pe>
2018-02-27 18:41:15 -0500

    Modifying

--- src/pe/edu/utp/javawebnovum/models/PackageDetail.java
+++ src/pe/edu/utp/javawebnovum/models/PackageDetail.java
@@ -20,39 +20,44 @@ public class PackageDetail {
         return package_id;
     }
 
-    public void setPackage_id(int package_id) {
+    public PackageDetail setPackage_id(int package_id) {
         this.package_id = package_id;
+        return  this;
     }
 
     public int getThematic_id() {
         return thematic_id;
     }
 
-    public void setThematic_id(int thematic_id) {
+    public PackageDetail setThematic_id(int thematic_id) {
         this.thematic_id = thematic_id;
+        return  this;
     }
 
     public int getUser_id() {
         return user_id;
     }
 
-    public void setUser_id(int user_id) {
+    public PackageDetail setUser_id(int user_id) {
         this.user_id = user_id;
+        return  this;
     }
 
     public float getTotal_price() {
         return total_price;
     }
 
-    public void setTotal_price(float total_price) {
+    public PackageDetail setTotal_price(float total_price) {
         this.total_price = total_price;
+        return  this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public PackageDetail setDescription(String description) {
         this.description = description;
+        return  this;
     }
 }
